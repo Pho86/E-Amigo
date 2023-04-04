2353bdda59b054ce1427e3b752fd3ef3ccbb8a08
Pho <pho86@my.bcit.ca>
2023-04-03 17:48:08 -0700

    fix profile pagse

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -63,6 +63,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
       },
       include: {
          user: true,
+         comments: true,
       }
    })
    return {
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -116,6 +116,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
       },
       include: {
          user: true,
+         comments:true,
       }
    })
    return {
